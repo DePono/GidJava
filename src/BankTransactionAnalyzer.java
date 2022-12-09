e01651d3a251b0113e643a19e2fc23597901eeda
pro-m <maestrodark@icloud.com>
2022-12-09 14:54:43 +0400

    Дописан BankTransactionAnalyzer

--- src/BankTransactionAnalyzer.java
+++ src/BankTransactionAnalyzer.java
@@ -2,14 +2,19 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 public class BankTransactionAnalyzer {
     public static final String RESOURCES = "";
+    private static final BankStatementCSVParser bankStatementParser = new BankStatementCSVParser();
 
     public static void main(String[] args) throws IOException {
-        final Path path = Paths.get(RESOURCES + args[0]);
+        String fileName = args[0];
+        final Path path = Paths.get(RESOURCES + fileName);
         List<String> lines = Files.readAllLines(path);
+        final List<BankTransaction> bankTransactions = bankStatementParser.parseLinesFromCSV(lines);
+        final BankStatementProcessor bankStatementProcessor = new BankStatementProcessor(bankTransactions);
         double total = 0d;
         for (final String line: lines) {
             final String[] columns = line.split(",");
